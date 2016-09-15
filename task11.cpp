38460a0e282fca2673e44d32595323ce9a682480
kundyzbekov <kundyzbekov98@gmail.com>
2016-09-15 10:43:12 +0600

    Add files via upload

---
+++ task11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+
+     int a,b,c,d;
+     cin>>a>>b>>c>>d;
+     if (a>=b && b>=c && c>=d) {
+              
+
+     cout<<a<<endl;}
+  
+     else if  (a>b && b>c && c>d) {}
+
+
+     
+     else {
+     cout<<a*a<<" "<<b*b<<" "<<" "<<c*c<<" "<<d*d<<endl;}
+ 
+
+
+
+
+ 
+system("pause>nul");
+return 0;
+}
